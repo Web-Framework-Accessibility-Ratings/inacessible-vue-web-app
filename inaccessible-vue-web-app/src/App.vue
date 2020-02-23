1ee00094c5f9b6df984ff2facd93990486cdf3c2
84longleym <84longleym@penguin>
2020-02-23 11:14:52 -0500

    Use of color violated

--- inaccessible-vue-web-app/src/App.vue
+++ inaccessible-vue-web-app/src/App.vue
@@ -8,6 +8,7 @@
       <button class="links" v-on:click="updateComponent('MeaningfulSequence')">Meaningul Sequence</button>
       <button class="links" v-on:click="updateComponent('InputPurpose')">Incorrect Input Purpose</button>
       <button class="links" v-on:click="updateComponent('IdentifyPurpose')">No Purpose on Elements</button>
+      <button class="links" v-on:click="updateComponent('UseOfColor')">No Use of Color</button>
     </div>
     <div class="inaccessibleContent">
       <component v-bind:is="currentComponent"/>
@@ -20,7 +21,8 @@ import ImgNoAlt from './components/1.1TextAlternatives.vue'
 import InfoRelationship from './components/1.3.1InfoRelationship.vue';
 import MeaningfulSequence from './components/1.3.2MeaningfulSequence.vue';
 import InputPurpose from './components/1.3.5InputPurpose.vue';
-import IdentifyPurpose from './components/1.3.6IdentifyPurpose.vue'
+import IdentifyPurpose from './components/1.3.6IdentifyPurpose.vue';
+import UseOfColor from './components/1.4.1UseOfColor';
 
 export default {
   name: 'App',
@@ -29,7 +31,8 @@ export default {
     InfoRelationship,
     MeaningfulSequence,
     InputPurpose,
-    IdentifyPurpose
+    IdentifyPurpose,
+    UseOfColor
   },
   data: function() {
     return {
